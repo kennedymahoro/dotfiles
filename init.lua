37548464ceea386ac5a8fc9f10db9da1e189ca00
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-07-10 22:11:49 -0400

    added a keymap for telescope find_file '<leader>f'

--- init.lua
+++ init.lua
@@ -263,6 +263,9 @@ vim.keymap.set({'n'}, '<leader>e', ":Neotree <enter>", {silent = true})
 -- Keymap for closing Neotree
 vim.keymap.set({'n'}, '<leader>o', ":Neotree close <enter>", {silent = true})
 
+-- keymap for find telescope find_files
+vim.keymap.set({'n'}, '<leader>f', ":Telescope find_files  <enter>", {silent = true})
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
@@ -317,11 +320,11 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'java'},
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
-
+  autotag = {enable = true},
   highlight = { enable = true },
   indent = { enable = true },
   incremental_selection = {
@@ -521,6 +524,7 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
